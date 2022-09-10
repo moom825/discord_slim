e666f76bb9c347b9888c40da603c71503eeb80c8
moom825 <maximusckaz@gmail.com>
2022-09-10 01:45:57 -0400

    still testing

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     install_requires=['aiohttp==3.7.4.post0','httpx==0.22.0','websockets==10.1'],
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
-    packages=find_packages('src'),
+    packages=['discord_slim/classes'],
     package_dir={'': 'src'},
 )
